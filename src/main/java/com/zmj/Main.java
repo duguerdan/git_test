9dc99a68189ac0171b7c5df76a3d78758aebf100
zmj <1312970787@qq.com>
2024-07-04 23:30:06 +0800

    创建项目

---
+++ src/main/java/com/zmj/Main.java
@@ -0,0 +1,11 @@
+package com.zmj;
+
+/**
+ * @author zhangmingjian
+ * @date 2024/7/4 23:18
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
